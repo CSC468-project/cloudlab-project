91e51f89cf2de76b804e7b6b7e4dba28842b7b5a
Evan Older <evanmolder@gmail.com>
2022-02-01 09:27:39 -0500

    Update profile.py
    Fixed setup execution syntax

--- profile.py
+++ profile.py
@@ -17,7 +17,7 @@ for node in nodes:
   node.addService(rspec.Execute(shell="/bin/sh", command="apt update && sudo apt upgrade -y"))
   node.addService(rspec.Execute(shell="/bin/sh", command=updstr))
   node.addService(rspec.Execute(shell="/bin/sh", command="git clone https://github.com/eolder/CSC468-WCU.git;mv cloudlab-project repo"))
-  node.addService(rspec.Execute(shell="/bin/sh", command="tmux new-session -d -s kubernetes 'bash repo/setup"))
+  node.addService(rspec.Execute(shell="/bin/sh", command="tmux new-session -d -s kubernetes 'bash repo/setup'"))
 
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
