8fa337e033fd28aaebdcf7052b03a02443345802
Evan Older <evanmolder@gmail.com>
2022-02-01 08:52:09 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -16,8 +16,8 @@ for node in nodes:
   node.routable_control_ip = "true"
   node.addService(rspec.Execute(shell="/bin/sh", command="apt update && sudo apt upgrade -y"))
   node.addService(rspec.Execute(shell="/bin/sh", command=updstr))
-  node.addService(rspec.Execute(shell="/bin/sh", command="git clone https://github.com/eolder/CSC468-WCU.git"))
-  node.addService(rspec.Execute(shell="/bin/sh", command="tmux new-session -d -s kubernetes 'bash CSC468-WCU/setup"))
+  node.addService(rspec.Execute(shell="/bin/sh", command="git clone https://github.com/eolder/CSC468-WCU.git;mv cloudlab-project repo"))
+  node.addService(rspec.Execute(shell="/bin/sh", command="tmux new-session -d -s kubernetes 'bash repo/setup"))
 
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
