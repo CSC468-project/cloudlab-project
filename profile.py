87742ed21b570b4c72c7f1daab18b02c76c1b2cf
Evan Older <evanmolder@gmail.com>
2022-03-29 16:22:00 -0400

    Initial testing of changes.

--- profile.py
+++ profile.py
@@ -6,13 +6,15 @@ import geni.rspec.pg as rspec
 request = portal.context.makeRequestRSpec()
 
 # Create node requests
-nodes = [request.XenVM("node")]
-nodes[0].cores = 4
+nodes = [request.XenVM("ctl"), request.XenVM("worker01")]
+nodes[0].cores = 1
+nodes[1].cores = 2
 
 # Create the nodes and handle updates and initial setup
 for node in nodes:
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
   node.routable_control_ip = "true"
+  # Launch a tmux session and within that run the setup script from the repo root
   node.addService(rspec.Execute(shell="bash", command="sudo tmux new-session -d -s setup 'sudo bash /local/repository/setup;bash -i'"))
 
 # Print the RSpec to the enclosing page.
