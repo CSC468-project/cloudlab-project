b6b6775a719137458d5282c8db505c3ec5f9a5ca
Evan Older <evanmolder@gmail.com>
2022-03-31 08:52:46 -0400

    Switching to Ngo's work.

--- profile.py
+++ profile.py
@@ -1,20 +1,51 @@
 import geni.portal as portal
-import geni.rspec.pg as rspec
+import geni.rspec.pg as pg
+import geni.rspec.igext as IG
+   
+pc = portal.Context()
+request = pc.makeRequestRSpec()
 
-# Create a Request object to start building the RSpec.
-# See https://docs.cloudlab.us/geni-lib.html for more details on this api
-request = portal.context.makeRequestRSpec()
+tourDescription = \
+"""
+This profile provides the template for 3 compute nodes with Docker and Kubernetes installed on Ubuntu 18.04
+"""
 
-# Create node requests
-nodes = [request.XenVM("ctl"), request.XenVM("worker01")]
+#
+# Setup the Tour info with the above description and instructions.
+#  
+tour = IG.Tour()
+tour.Description(IG.Tour.TEXT,tourDescription)
+request.addTour(tour)
 
-# Create the nodes and handle updates and initial setup
-for node in nodes:
-  node.cores = 2
-  node.ram = 2048
+prefixForIP = "192.168.1."
+link = request.LAN("lan")
+
+num_nodes = 3
+for i in range(num_nodes):
+  if i == 0:
+    node = request.XenVM("head")
+    bs_landing = node.Blockstore("bs_image", "/image")
+    bs_landing.size = "500GB"
+  else:
+    node = request.XenVM("worker-" + str(i))
+  node.cores = 4
+  node.ram = 8192
+  node.routable_control_ip = "true" 
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-  node.routable_control_ip = "true"
-  node.addService(rspec.Execute(shell="bash", command="sudo tmux new-session -d -s setup 'sudo bash /local/repository/setup;bash -i'"))
-
-# Print the RSpec to the enclosing page.
-portal.context.printRequestRSpec()
+  iface = node.addInterface("if" + str(i))
+  iface.component_id = "eth1"
+  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+  link.addInterface(iface)
+  
+  # setup Docker
+  node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
+  # setup Kubernetes
+  node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_kubernetes.sh"))
+  node.addService(pg.Execute(shell="sh", command="sudo swapoff -a"))
+  
+  if i == 0:
+    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/kube_manager.sh"))
+  else:
+    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/kube_worker.sh"))
+    
+pc.printRequestRSpec(request)
