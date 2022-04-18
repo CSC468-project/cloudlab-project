56540c8215f2ddb3d68a8ff1ff9726bdc332488e
Joe <josephrmcdowell@gmail.com>
2022-04-17 20:07:38 -0400

    From official documentation

--- DockerFiles/flaskct_data/database.py
+++ DockerFiles/flaskct_data/database.py
@@ -1,59 +1,57 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('mysql://root:mysql_root_123@127.0.0.1:3306/db')
-Base_session = scoped_session(sessionmaker(autocommit=False,
+db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
 Base = declarative_base()
-Base.query = Base_session.query_property()
+Base.query = db_session.query_property()
 
-def init_Base():
+def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
-    # you will have to import them first before calling init_Base()
-    # import yourapplication.models
+    # you will have to import them first before calling init_db()
+    import models
     Base.metadata.create_all(bind=engine)
 
 
 
 
-
-class Customer(Base.Model):
+""" Please dont remove this yet lol
+class Customer(db.Model):
     __tablename__ = 'customer'
-    id = Base.Column(Base.Integer, primary_key=True)
-    first_name = Base.Column(Base.String(50), nullable = False)
-    last_name = Base.Column(Base.String(50), nullable = False)
-    address = Base.Column(Base.String(500), nullable = False)
-    city = Base.Column(Base.String(50), nullable = False)
-    postcode = Base.Column(Base.String(50), nullable = False)
-    email = Base.Column(Base.String(50), nullable = False, unique = True)
-    phone_number = Base.Column(Base.String(50), nullable = False, unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable = False)
+    last_name = db.Column(db.String(50), nullable = False)
+    address = db.Column(db.String(500), nullable = False)
+    city = db.Column(db.String(50), nullable = False)
+    postcode = db.Column(db.String(50), nullable = False)
+    email = db.Column(db.String(50), nullable = False, unique = True)
+    phone_number = db.Column(db.String(50), nullable = False, unique = True)
 
-    orders = Base.relationship('Order', backref = 'customer')
+    orders = db.relationship('Order', backref = 'customer')
 
-order_menu = Base.Table('order_menu',
-    Base.Column('order_id', Base.Integer, Base.ForeignKey('order.id'), primary_key = True),
-    Base.Column('menu_id', Base.Integer, Base.ForeignKey('menu.id'), primary_key = True)
+order_menu = db.Table('order_menu',
+    db.Column('order_id', db.Integer, db.ForeignKey('order.id'), primary_key = True),
+    db.Column('menu_id', db.Integer, db.ForeignKey('menu.id'), primary_key = True)
 )
 
-class Order(Base.Model):
+class Order(db.Model):
     __tablename__ = 'order'
-    id = Base.Column(Base.Integer, primary_key=True)
-    time = Base.Column(Base.DateTime, nullable = False, default = datetime.utcnow)
-    sub_total = Base.Column(Base.Integer)
-    tip = Base.Column(Base.Integer)
-    total = Base.Column(Base.Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    sub_total = db.Column(db.Integer)
+    tip = db.Column(db.Integer)
+    total = db.Column(db.Integer)
     
-    customer_id = Base.Column(Base.Integer, Base.ForeignKey('customer.id'), nullable = False)
-    menu = Base.relationship('Menu', secondary = order_menu)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable = False)
+    menu = db.relationship('Menu', secondary = order_menu)
 
-class Menu(Base.Model):
+class Menu(db.Model):
     __tablename__ = 'menu'
-    id = Base.Column(Base.Integer, primary_key=True)
-    name = Base.Column(Base.String(100), nullable = False, unique = True)
-    price = Base.Column(Base.Integer, nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable = False, unique = True)
+    price = db.Column(db.Integer, nullable = False)
+"""
