47a82e66c8322bd3c7ea7de99493f5cc6c3cce4b
Evan Older <evanmolder@gmail.com>
2022-04-19 16:20:46 -0400

    Attempting to fix error.

--- DockerFiles/flaskct_data/database.py
+++ DockerFiles/flaskct_data/database.py
@@ -14,8 +14,8 @@ def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
-    Base.metadata.create_all(bind=engine)
     from models import Menu
+    Base.metadata.create_all(bind=engine)
     items = [Menu("Pizza", "3.50"), Menu("Fries", "$2.50"), Menu("Soda", "$1.75")]
     for item in items:
         db_session.add(item)
