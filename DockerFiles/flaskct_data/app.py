f93cc1a8b501d2eeb2629c51dee984067f514f4d
kdpetro <78497973+kdpetro@users.noreply.github.com>
2022-05-04 23:51:31 -0400

    Update app.py

--- DockerFiles/flaskct_data/app.py
+++ DockerFiles/flaskct_data/app.py
@@ -5,29 +5,9 @@ import sys
 app = Flask(__name__)
 
 
-def do_something(text1, text2):
-    text1 = text1.upper()
-    text2 = text2.upper()
-    combine = text1 + "+" + text2
-    return combine
-
-
 @app.route('/')
 def home():
     return render_template('home.html')
-
-
-@app.route('/join', methods=['GET', 'POST'])
-def my_form_post():
-    text1 = request.form['text1']
-    word = request.args.get('text1')
-    text2 = request.form['text2']
-    combine = do_something(text1, text2)
-    result = {
-        "output": combine
-    }
-    result = {str(key): value for key, value in result.items()}
-    return jsonify(result=result)
 
 
 @app.route('/menu', methods=['GET', 'POST'])
